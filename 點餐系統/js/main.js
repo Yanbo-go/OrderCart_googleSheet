9d2eba3cfd4e6b31646e6b546b7f567eff7dc145
Yanbo <31982613+Yanbo-go@users.noreply.github.com>
2022-08-27 00:12:23 +0800

    new

--- 點餐系統/js/main.js
+++ 點餐系統/js/main.js
@@ -1,15 +1,30 @@
 
   const orderS = document.querySelectorAll('select')[0];
-  const orderSS = document.querySelectorAll('select')[1];
+  //const orderSS = document.querySelectorAll('select')[1];
   const namev = document.querySelector('input');
-
-  var menu = ["----想來點雞----", "香酥雞腿便飯:", "香酥雞排便飯:", "香酥雞排飯:", "香滷大雞飯:", "蔥油雞飯:", "椒麻雞腿飯:", "香烤雞腿飯:", "香烤雞排飯:", "三杯雞飯:", "----想來點豬----", "手打排骨飯:", "香滷排骨飯:", "控肉飯:", "老宅鹹豬肉:", "私藏豬腳飯:", "--想來點魚--", "土魠魚飯:", "香酥鱈魚飯:", "日式鯖魚飯:", "椒鹽虱目魚:", "蒲燒鯛魚飯:", "香烤鯖魚飯:", "----想來點不一樣的----", "蒜泥白肉飯:", "叉燒雙寶飯:", "香腸飯:", "酥炸燒肉飯:", "蝦捲飯:", "花枝燒:"];
+  // 菜單
+  var menu = ["----想來點雞----", "香酥雞腿飯:", "香酥雞排飯:", "香滷雞排飯:", "香滷大雞腿:", "蔥油雞飯:", "椒麻雞腿飯:", "香烤雞腿飯:", "香烤雞排飯:", "三杯雞飯:", "----想來點豬----", "手打排骨飯:", "香滷排骨飯:", "控肉飯:", "老宅鹹豬肉:", "私藏豬腳飯:", "----想來點魚----", "土魠魚飯:", "香酥鱈魚飯:", "日式鯖魚飯:", "椒鹽虱目魚:", "蒲燒鯛魚飯:", "香烤鯖魚飯:", "----想來點不一樣的----", "蒜泥白肉飯:", "叉燒雙寶飯:", "香腸飯:", "酥炸燒肉飯:", "蝦捲飯:", "花枝燒:"];
+  // 價錢
   var price = ["", "105元", "100元", "100元", "105元", "105元", "110元", "110元", "105元", "110元", "", "90元", "90元", "90元", "90元", "105元", "", "95元", "95元", "95元", "95元", "95元", "100元", "", "85元", "90元", "90元", "90元", "95元", "90元"];
   
-
+  // 網站監聽載入時呼叫建立菜單Function
+  window.addEventListener('load', limitTime);
   window.addEventListener('load', crateMenulist);
+  // 限制時間
+  function limitTime() {
+    var currentTime = new Date();
+    var setTime = new Date(currentTime.getFullYear(),currentTime.getMonth(),
+    currentTime.getDate(),19,31,0 );
+    var urll = "../img/1668366.jpg";          
+    if(currentTime.getTime() > setTime.getTime()){
+      document.body.innerHTML="";
+      alert("目前不在使用時段(截止時間為10:30)，下次請早~\n"+currentTime);
+      document.body.setAttribute("background", urll);
 
 
+    };
+  }
+　// 建立菜單列表
   function crateMenulist() {
     for (var i = 0; i < menu.length; i++) {
       var op = document.createElement("option");
@@ -21,62 +36,53 @@
       op.setAttribute("id", "op" + i);
     }
   }
+
 　　
 
  $(function(){
         // 監聽 送出訂單 按鈕點擊
-        // 下面這段主要在組合資料，還有擋使用者不填資料不能送出
         $('#sendOrder').click(function(e){
         var status = true;
         // 姓名
         var name = $('#name').val();
-        // 電話
-        var total = $('#number2').val();
-       // var phone = $('#phone').val(); 
-        // 訂單  後面再加上選項數量*價格
+        // 數量
+        //var total = $('#number2').val();
+        // 訂單
         var order = '';
-        // 價格 先預設0 後面再加上
-        var price = 0;
-        var price2 = "";
+        // 價錢
+        var price = "";
         
-       /* $('input').focus(function(){
-          $(this).css('border','');
-        });*/
-        // 處理價格 跟 訂單 資料
+        // 處理價錢 跟 訂單 資料
         $('#number').each(function(index) {
           var ses = document.getElementById("number");
           if($(this).val() !== ''){
-            order = String(ses.options[ses.selectedIndex].text) + /*' - ' + $(this).val() +*/ "-" +  total +'份 ';
+            order = String(ses.options[ses.selectedIndex].text) + "-" + '1份 ';
             price += parseInt($(this).val());
           }
         });
         //order = order.substring(0, order.length - 1);
-        $('#number2').each(function(index) {
+        /*$('#number2').each(function(index) {
           if($(this).val() !== ''){
             var totalPrice = total * price;
             price2 += price + "x" + total + "=" + totalPrice + '元';
           }
-        });
-        // 擋住不填資料邏輯
+        });*/
+        // 擋住不填資料
         if(name == ''){
           $('#name').css('border','1px solid #ff0000');
           status = false;
         }
-        /*if(phone == ''){
-          $('#phone').css('border','1px solid #ff0000');
-          status = false;
-        }*/
         if(order == ''  ){
         alert('請選擇訂購品項喔');
         status = false;
         }
-        if(total == ''  ){
-        alert('請選擇數量喔');
-        status = false;
-        }
-        // 如果 �必填欄位都過了 才會到這邊
+        //if(total == ''  ){
+        //alert('請選擇數量喔');
+        //status = false;
+        //}
+
         if(status){
-          // 增加日期資料
+          // 日期資料
           var currentdate = new Date();
           var filltime = currentdate.getFullYear() + "/"
                 + (currentdate.getMonth() + 1) + "/"
@@ -84,13 +90,13 @@
                 + currentdate.getHours() + ":"
                 + currentdate.getMinutes() + ":"
                 + currentdate.getSeconds();
-          // 打包 要的資料
+          // 打包 
           var data = {
             'name' : name,
             //'phone':phone,
             'time': filltime,
             'order': order,
-            'price': price2,
+            'price': price,
           }
           // 呼叫 send ajax function
           send(data);
@@ -102,21 +108,21 @@
 
     function send(data){
       $.ajax({
-        // 這邊用get type
+        // get type
         type: "get",
-        // api url - google appscript 產出的 url
+        // api url
         url: "https://script.google.com/macros/s/AKfycbzv24Kx8LYwpHbHjHpITM05p3fnQiCKF50HX90H0WxvROueX6mBPHy3fpG5F48cbuV2uA/exec",
-        // 剛剛整理好的資料帶入
+        // 資料帶入
         data: data,
-        // 資料格式是JSON 
+        // 資料格式JSON 
         dataType: "JSON",
-        // 成功送出 會回頭觸發下面這塊感謝
         success: function (response) {
           console.log(response);
           alert('點餐完成！！');
+        // 清空欄位 
           namev.value = "";
           orderS.value ="";
-          orderSS.value ="";
+          //orderSS.value ="";
         }
       });
       
--- 點餐系統/js/main2.js
+++ 點餐系統/js/main2.js
@@ -1,22 +1,38 @@
 
   const orderS = document.querySelectorAll('select')[0];
-  const orderSSS = document.querySelectorAll('select')[1];
-  const orderSS = document.querySelectorAll('select')[2];
+  //const orderSSS = document.querySelectorAll('select')[1];
+  //const orderSS = document.querySelectorAll('select')[2];
   const namev = document.querySelectorAll('input')[0];
   //const name2v = document.querySelectorAll('input')[1];
-
-var menu =["1號-大麥克:","2號-雙層牛肉吉士堡:","3號-安格斯黑牛堡:","4號-嫩煎雞腿堡:","5號-麥香雞:","6號-麥克雞塊(6塊):","7號-麥克雞塊(10塊):",
+// 菜單
+var menu =["---一般套餐---","1號-大麥克:","2號-雙層牛肉吉士堡:","3號-安格斯黑牛堡:","4號-嫩煎雞腿堡:","5號-麥香雞:","6號-麥克雞塊(6塊):","7號-麥克雞塊(10塊):",
           "8號-勁辣雞腿堡:","9號-麥脆雞腿(2塊):","10號-麥脆雞翅(2塊):","11號-黃金起司豬排堡:","12號-麥香魚:","13號-千島黃金蝦堡:","----極選系列----","14號-BLT安格斯黑牛堡:",
           "15號-BLT辣脆雞腿堡:","16號-BLT嫩煎雞腿堡:","17號-BLT暈菇安格斯黑牛堡:","18號-凱薩脆雞沙拉:","19號-義式烤雞沙拉:",];
-var price =["130元","120元","","135元","100元","119元","159元","130元","175元","153元","","104元","",
+// 價錢
+var price =["","130元","120元","","135元","100元","119元","159元","130元","175元","153元","","104元","",
             "","169元","169元","169元","179元","159元","159元"];
 var submenu =["不升級","薯條加大","飲料加大","都加大"];
 var subprice =["0元","13元","7元","20元"];
 
-
+  // 網站監聽載入時呼叫建立菜單Function
+  window.addEventListener('load', limitTime);
   window.addEventListener('load', crateMenulist);
-  window.addEventListener('load', cratesubMenulist);
+  //window.addEventListener('load', cratesubMenulist);
+  // 限制時間
+  function limitTime() {
+    var currentTime = new Date();
+    var setTime = new Date(currentTime.getFullYear(),currentTime.getMonth(),
+    currentTime.getDate(),19,30,0 );
+    var urll = "../img/1668366.jpg";          
+    if(currentTime.getTime() > setTime.getTime()){
+      document.body.innerHTML="";
+      alert("目前不在使用時段(截止時間為10:30)，下次請早~\n"+currentTime);
+      document.body.setAttribute("background", urll);
 
+
+    };
+  }
+  // 建立菜單列表
   function crateMenulist(){
   for(i = 0 ;i < menu.length; i++){
    var op = document.createElement("option");
@@ -27,7 +43,7 @@ var subprice =["0元","13元","7元","20元"];
    op.setAttribute("id","opm" + i );
   }
 }
-  function cratesubMenulist(){
+  /*function cratesubMenulist(){
   for(i = 0 ;i < submenu.length; i++){
    var op = document.createElement("option");
    op.appendChild(document.createTextNode(submenu[i]+subprice[i]));   
@@ -36,55 +52,41 @@ var subprice =["0元","13元","7元","20元"];
    op.innerHtml = submenu[i]+subprice[i];
    op.setAttribute("id","sopm" + i );
   }
-}
+}*/
 
   $(function(){
         // 監聽 送出訂單 按鈕點擊
-        // 下面這段主要在組合資料，還有擋使用者不填資料不能送出
         $('#sendOrder').click(function(e){
         var status = true;
         // 姓名
         var name = $('#name').val();
         // 價錢
         //var price1 = $('#price').val();
-        // 電話
-        var total = $('#number2').val();
-        var total2 = $('#number3').val();
-       // var phone = $('#phone').val(); 
-        // 訂單  後面再加上選項數量*價格
+        //var total = $('#number2').val();
+        //var total2 = $('#number3').val();
+        // 訂單
         var order = '';
         //var order2 = '';
-        // 價格 先預設0 後面再加上
-        var price = 0;
-        var price2 = '';
-       /* $('input').focus(function(){
-          $(this).css('border','');
-        });*/
-        
+        // 價格
+        var price = 5;
         // 處理價格 跟 訂單 資料
         $('#number').each(function(index) {
           var ses = document.getElementById("number");
-          var ses2 = document.getElementById("number3");
-          if($('#number3').val == "請選擇升級"){
-            order = String(ses.options[ses.selectedIndex].text)   
-             + "-" +  total +'份 ';
-            price += parseInt($(this).val()) + parseInt(total2) ;
-          }
-          else if($(this).val() !== ''){
-            order = String(ses.options[ses.selectedIndex].text) + "-" +  
-            String(ses2.options[ses2.selectedIndex].text) +"-" +  total +'份 ';
-            price += parseInt($(this).val()) + parseInt(total2) ;
+          //var ses2 = document.getElementById("number3");
+          if($(this).val() !== ''){
+            order = String(ses.options[ses.selectedIndex].text)+ "x1份";
+            price += parseInt($(this).val());
           }
         });
-        order = order.substring(0, order.length - 1);
-        console.log(total2);
+        //order = order.substring(0, order.length - 1);
+        
 
-        $('#number2').each(function(index) {
+        /*$('#number2').each(function(index) {
           if($(this).val() !== ''){
             var totalPrice2 = total * price;
             price2 += price + "x" + total + "=" + totalPrice2 + '元';
           }
-        });
+        });*/
 
         
 
@@ -93,10 +95,6 @@ var subprice =["0元","13元","7元","20元"];
           $('#name').css('border','1px solid #ff0000');
           status = false;
         }
-        /*if(phone == ''){
-          $('#phone').css('border','1px solid #ff0000');
-          status = false;
-        }*/
         /*if(price1 == ''){
           $('#price').css('border','1px solid #ff0000');
           status = false;
@@ -105,13 +103,9 @@ var subprice =["0元","13元","7元","20元"];
         alert('請選擇訂購品項喔');
         status = false;
         }
-        if(total == ''  ){
-        alert('請選擇數量喔');
-        status = false;
-        }
-        // 如果 �必填欄位都過了 才會到這邊
+        // 如果必填欄位都過了 才會到這邊
         if(status){
-          // 增加日期資料
+          // 日期資料
           var currentdate = new Date();
           var filltime = currentdate.getFullYear() + "/"
                 + (currentdate.getMonth() + 1) + "/"
@@ -119,13 +113,13 @@ var subprice =["0元","13元","7元","20元"];
                 + currentdate.getHours() + ":"
                 + currentdate.getMinutes() + ":"
                 + currentdate.getSeconds();
-          // 打包 要的資料
+          // 打包
           var data = {
             'name' : name,
             //'phone':phone,
             'time': filltime,
             'order': order,
-            'price': price2,
+            'price': price,
           }
           // 呼叫 send ajax function
           send(data);
@@ -137,21 +131,20 @@ var subprice =["0元","13元","7元","20元"];
 
     function send(data){
       $.ajax({
-        // 這邊用get type
+        // get type
         type: "get",
-        // api url - google appscript 產出的 url
+        // api url
         url: "https://script.google.com/macros/s/AKfycbzv24Kx8LYwpHbHjHpITM05p3fnQiCKF50HX90H0WxvROueX6mBPHy3fpG5F48cbuV2uA/exec",
-        // 剛剛整理好的資料帶入
+        // 資料帶入
         data: data,
-        // 資料格式是JSON 
+        // 資料格式JSON 
         dataType: "JSON",
-        // 成功送出 會回頭觸發下面這塊感謝
         success: function (response) {
           console.log(response);
           alert('點餐完成！！');
+        // 清空欄位 
           namev.value = "";
           orderS.value ="";
-          orderSS.value ="";
         }
       });
       
